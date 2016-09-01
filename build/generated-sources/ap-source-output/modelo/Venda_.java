081d59fe3eb5b1bfb834019fa9f70ee660d92b98
ThalesEu <Thalesbages@gmail.com>
2016-09-01 20:05:55 -0300

    Thales Comercio
    CSS starwars

---
+++ build/generated-sources/ap-source-output/modelo/Venda_.java
@@ -0,0 +1,18 @@
+package modelo;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-01T01:06:28")
+@StaticMetamodel(Venda.class)
+public class Venda_ { 
+
+    public static volatile SingularAttribute<Venda, Integer> codcliente;
+    public static volatile SingularAttribute<Venda, Integer> codigo;
+    public static volatile SingularAttribute<Venda, Double> total;
+    public static volatile SingularAttribute<Venda, Date> datavenda;
+    public static volatile SingularAttribute<Venda, Integer> codstatus;
+
+}
